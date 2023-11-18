4289ab5ed624a0b10fb0aba5afe011c53e79cc3f
FTKV <mthkvv@gmail.com>
2023-11-18 04:35:00 +0000

    fix create_all

--- src/database/create_all.py
+++ src/database/create_all.py
@@ -1,14 +1,31 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+# https://stackoverflow.com/questions/16981921/relative-imports-in-python-3/16985066#16985066
+
 import asyncio
 
-from connect_db import engine
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+)
+
+from conf.config import settings
 from models import Base
 
 
-async def async_main(engine) -> None:
+engine: AsyncEngine = create_async_engine(
+    settings.sqlalchemy_database_url_async,
+    echo=False,
+)
 
+
+async def async_main(engine) -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
     await engine.dispose()
 
 
